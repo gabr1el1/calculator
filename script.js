7074238c5a059ccabe74a8f27c6a7e7ad2da9e70
gabr1el1 <gabrielhgxd@gmail.com>
2023-03-02 08:36:58 -0600

    Operations more or less working

--- script.js
+++ script.js
@@ -1,28 +1,71 @@
-const numbers = Array.from(document.querySelectorAll(".number"));
 const screen = document.querySelector(".screen");
+const numbers = Array.from(document.querySelectorAll(".number"));
+const operations = Array.from(document.querySelectorAll(".operation"));
+
 let cursorPos, newValue;
+let result = 0;
+let ops="=";
+let clear;
+
 screen.addEventListener('keydown',function(e){
     if(isNaN(e.key) && e.keyCode!==8 && e.keyCode!==37 && e.keyCode!==39){
-        //console.log(e.keyCode)
         e.preventDefault();
+    } 
+    if(clear){
+        screen.value = "";
+        clear = false;
     }
-    
 });
 numbers.forEach(function(number){
-    
-    number.addEventListener('mouseup',function(){
-        if(screen.selectionEnd==0){
-            cursorPos = 0;
-        }else{
-            cursorPos = screen.selectionEnd;
-        }
-        newValue = screen.value.split("");
-        newValue.splice(cursorPos,0,number.innerText);
-        newValue = newValue.join("");
-        screen.value = newValue;
-        screen.focus();
-        //screen.setAttribute('type','number');
-
-        //console.log(cursorPos);
-    })
+    number.addEventListener('mouseup',function(){writeToScreen(number)});
 });
+operations.forEach(function(operator){
+    operator.addEventListener('mouseup',function(){makeOperation(operator)});
+});
+function makeOperation(operator){
+    console.log(ops)
+    operate(Number(screen.value));
+    ops = operator.innerText;
+}
+function writeToScreen(number){
+    if(clear){
+        screen.value = "";
+        clear = false;
+    }
+    if(screen.selectionEnd==0){
+        cursorPos = 0;
+    }else{
+        cursorPos = screen.selectionEnd;
+    }
+    newValue = screen.value.split("");
+    newValue.splice(cursorPos,0,number.innerText);
+    newValue = newValue.join("");
+    screen.value = newValue;
+    screen.focus();
+    screen.selectionEnd = cursorPos+1;
+}
+function operate(number){
+    clear = true;
+    switch(ops){
+        case "+":
+            result+=number;
+            screen.value = result;
+            break;
+        case "-":
+            result-=number;
+            screen.value = result;
+            break;
+        case "x":
+            result*=number;
+            screen.value = result;
+            break;
+        case "÷":
+            result/=number;
+            screen.value = result;
+            break;
+        case "=":
+            result = number;
+            screen.value = result;
+            break;
+    }
+}
